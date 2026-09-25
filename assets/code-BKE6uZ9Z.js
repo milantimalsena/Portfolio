c711002414aecc1880fcee60255ca2c587e60552
milantimalsena <milantimalsena@users.noreply.github.com>
2026-09-25 13:27:44 +0000

    deploy: c1c68778a5d53b8c2b470930190158f4f4e0bef6

---
+++ assets/code-BKE6uZ9Z.js
@@ -0,0 +1,1 @@
+import{c as e}from"./index-C0fDCGH5.js";const c=[["path",{d:"M16 20V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16",key:"jecpp"}],["rect",{width:"20",height:"14",x:"2",y:"6",rx:"2",key:"i6l2r4"}]],a=e("briefcase",c);const o=[["path",{d:"m16 18 6-6-6-6",key:"eg8j8"}],["path",{d:"m8 6-6 6 6 6",key:"ppft3o"}]],d=e("code",o);export{a as B,d as C};
